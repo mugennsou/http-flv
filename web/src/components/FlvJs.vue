e6f0d449ab20d10cb8788e66fddc65433af7401c
Mugen <mj501738931@126.com>
2018-09-06 15:16:00 +0800

    Updated nginx version.
    Updated alpine version.
    Updated nginx-http-flv-module to master branch.

---
+++ web/src/components/FlvJs.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="flv-js-player">
+    <el-form :model="mediaDataSource" :disabled="!supported">
+      <el-form-item>
+        <el-input v-model="mediaDataSource.url" />
+      </el-form-item>
+      <el-form-item>
+        <el-checkbox v-model="mediaDataSource.hasAudio" label="hasAudio" />
+        <el-checkbox v-model="mediaDataSource.hasVideo" label="hasVideo" />
+      </el-form-item>
+      <el-form-item>
+        <el-button size="mini" type="primary" @click="load">LOAD</el-button>
+        <el-button size="mini" @click="start">START</el-button>
+        <el-button size="mini" @click="pause">PAUSE</el-button>
+        <el-button size="mini" type="danger" @click="destroy">DESTROY</el-button>
+      </el-form-item>
+    </el-form>
+    <video controls ref="video">
+    </video>
+  </div>
+</template>
+
+<script>
+import Flv from 'flv.js'
+
+export default {
+  name: 'flv-js',
+  watch: {
+    'mediaDataSource.url': (value) => {
+      try {
+        localStorage.setItem('flv-js.mediaDataSource.url', value)
+      } catch (error) {
+        // eslint-disable-next-line
+        console.log(error)
+      }
+    }
+  },
+  methods: {
+    loadLocalMediaDataSource() {
+      try {
+        let url = localStorage.getItem('flv-js.mediaDataSource.url')
+        if (url !== null) this.mediaDataSource.url = url
+      } catch (error) {
+        // eslint-disable-next-line
+        console.log(error)
+      }
+    },
+    load() {
+      this.player && this.destroy()
+      this.player = Flv.createPlayer(this.mediaDataSource)
+      this.player.attachMediaElement(this.$refs.video);
+      this.player.load()
+
+      this.bindEvents()
+      this.start()
+    },
+    bindEvents() {
+      this.player.on('error', this.destroy)
+      this.player.on('loading_complete', this.destroy)
+      this.player.on('recovered_early_eof', this.destroy)
+      this.player.on('media_info', this.destroy)
+      this.player.on('metadata_arrived', this.destroy)
+      this.player.on('statistics_info', () => { })
+    },
+    start() {
+      this.player && this.player.play();
+    },
+    pause() {
+      this.player && this.player.pause();
+    },
+    destroy() {
+      if (this.player) {
+        this.player.pause();
+        this.player.unload();
+        this.player.detachMediaElement();
+        this.player.destroy();
+        this.player = null;
+      }
+    }
+  },
+  data() {
+    return {
+      supported: false,
+      player: null,
+      mediaDataSource: {
+        type: 'flv',
+        isLive: true,
+        url: 'http://localhost/live?app=demo',
+        hasAudio: true,
+        hasVideo: true,
+      }
+    }
+  },
+  mounted() {
+    this.supported = Flv.isSupported()
+
+    this.loadLocalMediaDataSource()
+  },
+  beforeDestroy() {
+    this.destroy()
+  }
+}
+</script>
+
+<style lang="scss">
+#flv-js-player {
+  > video {
+    width: 100%;
+  }
+}
+</style>
---
+++ web/src/views/Player.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-container id="player">
+    <el-main>
+      <el-row>
+        <el-col :lg="{span:12,offset:6}" :md="{span:16,offset:4}">
+          <el-tabs :value="defaultTab" @tab-click="handleClick">
+            <el-tab-pane v-for="(tab,index) in tabs" :key="index" :name="index" :label="tab.name" />
+          </el-tabs>
+          <component v-if="tabs[currentTab] || tabs[defaultTab]" :is="tabs[currentTab] || tabs[defaultTab]" />
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import FlvJs from '@/components/FlvJs'
+
+export default {
+  name: 'player',
+  components: {
+    FlvJs
+  },
+  computed: {
+    defaultTab() {
+      return Object.keys(this.tabs).length ? Object.keys(this.tabs)[0] : null;
+    },
+  },
+  methods: {
+    handleClick(tab) {
+      this.currentTab = tab.name
+    }
+  },
+  data() {
+    return {
+      tabs: {
+        FlvJs
+      },
+      currentTab: null
+    }
+  }
+}
+</script>
